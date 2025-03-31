3a74e6a59020699e2445360b00ae7b51d62d0d67
Fabry <sparviero46@hotmail.it>
2025-03-31 14:50:59 +0200

    pippo

---
+++ app.js
@@ -0,0 +1,20 @@
+function somma(uu, b) {
+    return uu + b;
+}
+console.log(somma(1, 2));
+console.log(somma(1, 2));
+console.log(somma(1, 2));
+console.log(somma(3, 2));
+console.log(somma(1, 2));
+console.log(somma(1, 2));
+var isCompleted;
+isCompleted = true;
+console.log("è stato completato:", isCompleted);
+var decimal = 10;
+var hex = 0xf;
+var binary = 4;
+console.log("kjkjkjk:", binary);
+var color = "bianco";
+var cosa = "manuel";
+var pippo = "ciao mi chiamo " + cosa + " e sono di colore " + color;
+console.log(pippo);
---
+++ app.ts
@@ -0,0 +1,25 @@
+function somma(uu: number, b: number) {
+  return uu + b;
+}
+console.log(somma(1, 2));
+console.log(somma(1, 2));
+console.log(somma(1, 2));
+console.log(somma(3, 2));
+console.log(somma(1, 2));
+console.log(somma(1, 2));
+
+let isCompleted: boolean;
+isCompleted = true;
+console.log("è stato completato:", isCompleted);
+
+let decimal: number = 10;
+
+let hex: number = 0xf;
+
+let binary: number = 0b100;
+
+console.log("kjkjkjk:", binary);
+let color: string = "bianco";
+let cosa: string = "manuel";
+let pippo: string = "ciao mi chiamo " + cosa + " e sono di colore " + color;
+console.log(pippo);
