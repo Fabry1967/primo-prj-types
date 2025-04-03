e5b53dd77bc33364ba211d065ae27fb1eaef9aaa
Fabry <sparviero46@hotmail.it>
2025-04-03 09:26:45 +0200

    Funzioni

--- app.js
+++ app.js
@@ -17,4 +17,132 @@ console.log("kjkjkjk:", binary);
 var color = "bianco";
 var cosa = "manuel";
 var pippo = "ciao mi chiamo " + cosa + " e sono di colore " + color;
-console.log(pippo);
+console.log("jjjjj ${pippo}");
+var pippo2 = ["ciao mi chiamo ", "kk", "ooooo"];
+console.table(pippo2.join(", "));
+var devices = [
+    {
+        name: "iPhone",
+        brand: "Apple",
+    },
+    {
+        name: "Galaxy",
+        brand: "Samsung",
+    },
+];
+console.table(devices);
+//tuple
+var persona = ["Mario", 30];
+console.log("Nome:", persona[0]);
+console.log("Età:", persona[1]);
+//enum
+var Colore;
+(function (Colore) {
+    Colore[Colore["bianco"] = 0] = "bianco";
+    Colore[Colore["nero"] = 1] = "nero";
+    Colore[Colore["blu"] = 2] = "blu";
+})(Colore || (Colore = {}));
+var colorePreferito = Colore.bianco;
+console.log("Colore preferito:", colorePreferito);
+console.log("Colore preferito:", Colore[0]);
+console.log("Colore preferito:", Colore[1]);
+console.log("Colore preferito:", Colore[2]);
+//Any
+var valorecasuale = 100;
+var dd = ["ciao", "pippo", "pluto"];
+valorecasuale = dd[0];
+console.log("valorecasuale:", valorecasuale);
+//void
+function saluta(nome, cognome) {
+    console.log("Ciao ".concat(nome, " ").concat(cognome));
+}
+saluta("hhh", "jjj");
+//null e undefined
+var u = null;
+var v = undefined;
+console.log("u:", u);
+console.log("v:", v);
+var misto = "ciao";
+misto = 100;
+console.log("misto:", misto);
+misto = null;
+console.log("misto:", misto);
+misto = "jj";
+console.log("misto:", misto);
+//type aliasies
+var plyt;
+var employee1 = {
+    nome: "Mario",
+    cognome: "Rossi",
+    etax: 30,
+    id: 1,
+    nome2: "Mario",
+};
+plyt = employee1;
+console.log("plyt:", plyt);
+var clickevent = "click";
+//type guard
+function isnumber(value) {
+    return typeof value === "number";
+}
+if (isnumber(100)) {
+    console.log("è un numero");
+}
+function isString(value) {
+    return typeof value === "string";
+}
+if (isString("ciao")) {
+    console.log("è una stringa");
+}
+else {
+    console.log("è un numero", isnumber(100));
+}
+var Uccello = /** @class */ (function () {
+    function Uccello() {
+    }
+    Uccello.prototype.vola = function () {
+        console.log("vola");
+    };
+    return Uccello;
+}());
+var Pesce = /** @class */ (function () {
+    function Pesce() {
+    }
+    Pesce.prototype.nuota = function () {
+        console.log("nuota");
+    };
+    return Pesce;
+}());
+function muoviAnimale(animale) {
+    if (animale instanceof Uccello) {
+        animale.vola();
+    }
+    else {
+        animale.nuota();
+    }
+}
+//nullable e undefined
+var x = 6;
+function d(x) {
+    if (x === null) {
+        return 0;
+    }
+    else {
+        return x * 2;
+    }
+}
+console.log("d:", d(x));
+//type assertion
+var userInput = document.getElementById("user-Input");
+if (userInput) {
+    userInput.min = "2";
+}
+//let pippo3 = getUserData() as User;
+//pippo3.cognome = "Rossi";
+//generics da vedere poi
+//FUNZIONI+++++++++++++++
+function saluta4(nome, cognome) {
+    return "Ciao ".concat(nome, " ").concat(cognome);
+}
+console.log(saluta4("Mario"));
+saluta4("Mario", "Rossi");
--- app.ts
+++ app.ts
@@ -22,4 +22,154 @@ console.log("kjkjkjk:", binary);
 let color: string = "bianco";
 let cosa: string = "manuel";
 let pippo: string = "ciao mi chiamo " + cosa + " e sono di colore " + color;
-console.log(pippo);
+console.log("jjjjj ${pippo}");
+
+let pippo2: string[] = ["ciao mi chiamo ", "kk", "ooooo"];
+console.table(pippo2.join(", "));
+let devices = [
+  {
+    name: "iPhone",
+    brand: "Apple",
+  },
+  {
+    name: "Galaxy",
+    brand: "Samsung",
+  },
+];
+
+console.table(devices);
+//tuple
+let persona: [string, number] = ["Mario", 30];
+console.log("Nome:", persona[0]);
+console.log("Età:", persona[1]);
+
+//enum
+enum Colore {
+  bianco,
+  nero,
+  blu,
+}
+let colorePreferito: Colore = Colore.bianco;
+console.log("Colore preferito:", colorePreferito);
+console.log("Colore preferito:", Colore[0]);
+console.log("Colore preferito:", Colore[1]);
+console.log("Colore preferito:", Colore[2]);
+
+//Any
+let valorecasuale: any = 100;
+let dd: string[] = ["ciao", "pippo", "pluto"];
+valorecasuale = dd[0];
+console.log("valorecasuale:", valorecasuale);
+
+//void
+function saluta(nome: string, cognome: string): void {
+  console.log(`Ciao ${nome} ${cognome}`);
+}
+saluta("hhh", "jjj");
+
+//null e undefined
+let u: null = null;
+let v: undefined = undefined;
+console.log("u:", u);
+console.log("v:", v);
+
+let misto: string | number | null | undefined = "ciao";
+misto = 100;
+console.log("misto:", misto);
+misto = null;
+console.log("misto:", misto);
+misto = "jj";
+console.log("misto:", misto);
+
+interface Persona {
+  nome: string;
+  cognome?: string;
+  etax?: number;
+}
+interface Cosa {
+  id: number;
+  nome2: string;
+}
+
+type pippo = Persona & Cosa;
+//type aliasies
+let plyt: pippo;
+let employee1: pippo = {
+  nome: "Mario",
+  cognome: "Rossi",
+  etax: 30,
+  id: 1,
+  nome2: "Mario",
+};
+plyt = employee1;
+console.log("plyt:", plyt);
+
+//type literal
+type ClickEvent = "click" | "dblclick" | "mouseover";
+let clickevent: ClickEvent = "click";
+
+//type guard
+function isnumber(value: any): value is number {
+  return typeof value === "number";
+}
+if (isnumber(100)) {
+  console.log("è un numero");
+}
+function isString(value: any): value is string {
+  return typeof value === "string";
+}
+if (isString("ciao")) {
+  console.log("è una stringa");
+} else {
+  console.log("è un numero", isnumber(100));
+}
+
+class Uccello {
+  vola() {
+    console.log("vola");
+  }
+}
+class Pesce {
+  nuota() {
+    console.log("nuota");
+  }
+}
+function muoviAnimale(animale: Uccello | Pesce) {
+  if (animale instanceof Uccello) {
+    animale.vola();
+  } else {
+    animale.nuota();
+  }
+}
+//nullable e undefined
+let x: null | number = 6;
+
+function d(x: number | null): number {
+  if (x === null) {
+    return 0;
+  } else {
+    return x * 2;
+  }
+}
+console.log("d:", d(x));
+
+//type assertion
+let userInput = document.getElementById("user-Input") as HTMLInputElement;
+if (userInput) {
+  userInput.min = "2";
+}
+
+interface User {
+  name: string;
+  cognome: string;
+}
+
+// generics da vedere poi
+
+// FUNZIONI+++++++++++++++
+
+function saluta4(nome: string, cognome?: string): string {
+  return `Ciao ${nome} ${cognome}`;
+}
+console.log(saluta4("Mario"));
+saluta4("Mario", "Rossi");
